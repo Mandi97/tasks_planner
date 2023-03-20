32c69d44183353403d144cc64e94bbfc505c6192
Mandi97 <pawel.mandrysz@gmail.com>
2023-03-20 13:04:24 +0100

    edit planner, delete planner and create planner

--- planners/urls.py
+++ planners/urls.py
@@ -7,5 +7,8 @@ app_name = 'planners'
 urlpatterns = [
     path('', views.PlannerListView.as_view(), name='planners-list'),
     path('<int:pk>/', views.PlannerDetailView.as_view(), name='planner-detail'),
+    path('create/', views.PlannerCreateView.as_view(), name='planner-create'),
+    path('delete/<int:pk>', views.PlannerDeleteView.as_view(), name='planner-delete'),
+    path('update/<int:pk>', views.PlannerUpdateView.as_view(), name='planner-update')
 
 ]
--- planners/views.py
+++ planners/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, DeleteView
+from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
 
 from . import models
 
@@ -17,3 +17,32 @@ class PlannerDetailView(DetailView):
     model = models.Planner
     template_name = 'planners/planner_details.html'
     context_object_name = 'planner'
+
+
+class PlannerCreateView(LoginRequiredMixin, CreateView):
+    model = models.Planner
+    fields = ('title', 'slug')
+    template_name = 'planners/planner_create.html'
+    success_url = reverse_lazy('planners:planners-list')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class PlannerDeleteView(LoginRequiredMixin, DeleteView):
+    model = models.Planner
+    success_url = reverse_lazy('planners:planners-list')
+    template_name = 'planners/planner_delete.html'
+    context_object_name = 'planner'
+
+
+class PlannerUpdateView(LoginRequiredMixin, UpdateView):
+    model = models.Planner
+    fields = ('title', 'slug')
+    template_name = 'planners/planner_update.html'
+    context_object_name = 'planner'
+
+    def get_success_url(self):
+        pk = self.kwargs.get('pk')
+        return str(reverse_lazy('planners:planner-detail', kwargs={'pk': pk}))
