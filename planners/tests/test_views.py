498588f921f5365d1f47b5be081e888f8ada6950
Mandi97 <pawel.mandrysz@gmail.com>
2023-03-23 15:55:52 +0100

    add unit tests for create task page and all planners page

--- planners/tests/test_views.py
+++ planners/tests/test_views.py
@@ -17,3 +17,30 @@ def test_add_planner_page_without_logged_in_user(client):
     response = client.get(url)
 
     assert response.status_code == 302
+
+
+def test_all_planners_page_with_logged_in_user(client, user):
+    url = reverse('planners:planners-list')
+
+    client.login(email=user.email, password='testPass123')
+    response = client.get(url)
+
+    assert response.status_code == 200
+
+
+def test_create_task_page_with_logged_in_user(client, user):
+    url = reverse('planners:task-create')
+
+    client.login(email=user.email, password='testPass123')
+    response = client.get(url)
+
+    assert response.status_code == 200
+    assert '<h1>Create Task</h1>' in response.content.decode('UTF-8')
+
+
+def test_create_task_page_without_logged_in_user(client):
+    url = reverse('planners:task-create')
+
+    response = client.get(url)
+
+    assert response.status_code == 302
